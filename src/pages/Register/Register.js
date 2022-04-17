d884a2d72392f82c831c6e7fa526faa7c88f19a4
Rahulhawlader <hawladerrahul8@gmail.com>
2022-04-17 22:25:11 +0600

    register form

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Regiter.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = () => {
@@ -10,11 +19,13 @@ const Register = () => {
     }
 
     const handleRegister = (event) => {
-        event.preventdefault();
+        event.preventDefault();
+        console.log(event.target);
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(name, email, password)
+
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='register-form'>
