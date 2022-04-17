d184193f639a0c6f7bc8bec1e4536e3f4c9b2d19
Rahulhawlader <hawladerrahul8@gmail.com>
2022-04-17 12:30:29 +0600

    sevices part

--- src/pages/Home/Service/Service.js
+++ src/pages/Home/Service/Service.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({ service }) => {
     const { id, name, price, description, img } = service;
     return (
-        <div>
+        <div className='service-style'>
             <img src={img} alt=' ' />
             <h2>Name:{name}</h2>
             <h3>Price: ${price}</h3>
             <p>description: {description}</p>
+            <button>Check Out</button>
 
         </div>
     );
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,13 +12,15 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h1>My Services: {services.length}</h1>
-            {
-                services.map(service => <Service
-                    key={service.id}
-                    service={service}
-                ></Service>)
-            }
+            <h1 style={{ textAlign: "center" }}>My Services:-</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
